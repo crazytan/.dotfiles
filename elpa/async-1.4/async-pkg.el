cf6bafb8a088cc1cc169c7246f1623398b27a98e
crazytan <tjtanjia.tan@gmail.com>
2015-10-21 01:59:03 -0400

    init commit

---
+++ elpa/async-1.4/async-pkg.el
@@ -0,0 +1,1 @@
+(define-package "async" "1.4" "Asynchronous processing in Emacs" nil)
---
+++ elpa/magit-popup-2.2.2/magit-popup-pkg.el
@@ -0,0 +1,5 @@
+(define-package "magit-popup" "2.2.2"
+  "Define prefix-infix-suffix command combos"
+  '((emacs "24.4")
+    (async "1.4")
+    (dash "2.11.0")))
