0caa33bfd01e4e87d16fa0116b9db5e676d54694
crazytan <tjtanjia.tan@gmail.com>
2016-04-22 00:57:45 -0400

    projectile

--- init.el
+++ init.el
@@ -80,8 +80,15 @@
 ;; helm
 (require 'helm)
 (require 'helm-config)
-(helm-mode 1)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 (setq helm-buffers-fuzzy-matching t)
+(helm-mode 1)
+
+;; projectile
+(projectile-global-mode)
 
